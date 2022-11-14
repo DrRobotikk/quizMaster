a22d344a09ce8960f5868b724ea1f76cb55ff99e
DrRobotikk <90263786+DrRobotikk@users.noreply.github.com>
2022-11-14 11:17:56 +0100

    yes yes small change

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/about";
+import About from "./pages/About";
 import Quizes from "./pages/quizes";
 
 function App() {
